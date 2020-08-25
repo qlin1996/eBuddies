58cc59e04dc76407fc3bc46253704680ed05e28c
Blake Ferm <blakeferm@gmail.com>
2020-08-25 11:41:37 -0400

    updated imports

--- Frontend/containers/SingleEvent/SingleEventScreenStyle.js
+++ Frontend/containers/SingleEvent/SingleEventScreenStyle.js
@@ -1,5 +1,5 @@
-import { StyleSheet } from 'react-native'
-import { Helpers, Metrics, Fonts, Colors } from 'App/Theme'
+import { StyleSheet } from "react-native";
+import { Helpers, Metrics, Fonts, Colors } from "../../themes";
 
 export default StyleSheet.create({
   colMain: {
@@ -7,43 +7,43 @@ export default StyleSheet.create({
     width: 375,
     height: 667,
     backgroundColor: Colors.white,
-    overflow: 'hidden',
+    overflow: "hidden",
   },
   eventImg: {
     width: 375,
     height: 250,
     backgroundColor: Colors.secondary,
-    overflow: 'hidden',
+    overflow: "hidden",
   },
   eventTitle: {
     ...Fonts.h4,
     color: Colors.blue,
-    textAlign: 'left',
+    textAlign: "left",
     marginTop: 20,
     marginHorizontal: 20,
   },
   eventLocation: {
     ...Fonts.medium,
     color: Colors.blue,
-    textAlign: 'left',
+    textAlign: "left",
     marginHorizontal: 20,
   },
   locationIcon: {
     ...Fonts.large,
     color: Colors.blue,
-    textAlign: 'left',
+    textAlign: "left",
     marginHorizontal: 40,
   },
   eventDate: {
     ...Fonts.medium,
     color: Colors.blue,
-    textAlign: 'left',
+    textAlign: "left",
     marginHorizontal: 20,
   },
   eventDescription: {
     ...Fonts.h6,
     color: Colors.blue,
-    textAlign: 'left',
+    textAlign: "left",
     marginTop: 20,
     marginBottom: 20,
     marginHorizontal: 20,
@@ -51,7 +51,7 @@ export default StyleSheet.create({
   moreText: {
     ...Fonts.normal,
     color: Colors.text,
-    textAlign: 'left',
+    textAlign: "left",
     marginBottom: 20,
     marginHorizontal: 20,
     fontSize: 12,
@@ -62,8 +62,8 @@ export default StyleSheet.create({
     width: 50,
     borderRadius: 50,
     backgroundColor: Colors.primary,
-    alignItems: 'center',
-    marginLeft: 'auto',
+    alignItems: "center",
+    marginLeft: "auto",
     marginTop: 20,
     marginRight: 20,
   },
@@ -74,10 +74,10 @@ export default StyleSheet.create({
   },
   eventFooter: {
     width: 300,
-    alignContent: 'center',
-    justifyContent: 'center',
+    alignContent: "center",
+    justifyContent: "center",
     height: 70,
     backgroundColor: Colors.lightBlue,
     padding: Metrics.medium,
   },
-})
+});

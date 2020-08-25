e23c79e4bb627c91cc374837dd57110453c5d85e
Claire Giordano <cgiordano12@fordham.edu>
2020-08-24 21:56:00 -0400

    added sign up

---
+++ Frontend/containers/Signup/SignupScreen.js
@@ -0,0 +1,112 @@
+import React from "react";
+import { connect } from "react-redux";
+import { View, Text, Image, Button, TextInput, StyleSheet } from "react-native";
+import Style from "../AllEvents/AllEventsScreenStyle";
+import styles from "./SignUpScreenStyle";
+import { ApplicationStyles, Helpers, Metrics, Fonts } from "../../themes";
+
+class Signup extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      email: "",
+      password: "",
+    };
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.background}>
+          <View>
+            <Image
+              style={styles.arrow}
+              source={require("../../assets/arrow.png")}
+            />
+          </View>
+          <TextInput
+            style={{
+              ...Fonts.normal,
+              height: 50,
+              backgroundColor: "rgb(235, 233, 233)",
+              borderBottomWidth: 0.5,
+              borderBottomColor: "rgba(38,153,251,1)",
+              marginHorizontal: 40,
+              marginBottom: 20,
+              color: "rgba(38,153,251,1)",
+              paddingHorizontal: 10,
+            }}
+            onChangeText={(name) => this.setState({ name })}
+            value={this.state.name}
+            placeholder="John Doe"
+            placeholderTextColor="rgba(38,153,251,1)"
+            keyboardType="name-phone-pad"
+          />
+          <TextInput
+            style={{
+              ...Fonts.normal,
+              height: 50,
+              backgroundColor: "rgb(235, 233, 233)",
+              borderBottomWidth: 0.5,
+              borderBottomColor: "rgba(38,153,251,1)",
+              marginHorizontal: 40,
+              marginBottom: 20,
+              color: "rgba(38,153,251,1)",
+              paddingHorizontal: 10,
+            }}
+            onChangeText={(email) => this.setState({ email })}
+            value={this.state.email}
+            placeholder="jdoe@gmail.com"
+            placeholderTextColor="rgba(38,153,251,1)"
+            keyboardType="email-address"
+          />
+          <TextInput
+            style={{
+              ...Fonts.normal,
+              height: 50,
+              backgroundColor: "rgb(235, 233, 233)",
+              borderBottomWidth: 0.5,
+              borderBottomColor: "rgba(38,153,251,1)",
+              marginHorizontal: 40,
+              marginBottom: 20,
+              color: "rgba(38,153,251,1)",
+              paddingHorizontal: 10,
+            }}
+            onChangeText={(password) => this.setState({ password })}
+            value={this.state.password}
+            placeholder="Create Password"
+            placeholderTextColor="rgba(38,153,251,1)"
+            secureTextEntry
+          />
+          <View style={styles.button}>
+            <Button
+              color="white"
+              style={{ ...Fonts.normal, textAlign: "center" }}
+              title="CONTINUE"
+              onPress={this.onSignUp}
+            >
+              CONTINUE
+            </Button>
+          </View>
+          <View style={styles.account}>
+            <Button
+              color="rgba(38,153,251,1)"
+              style={{ ...Fonts.small }}
+              title="Already have an account?"
+            />
+          </View>
+          <View style={styles.login}>
+            <Button
+              color="rgba(38,153,251,1)"
+              style={{ ...Fonts.small }}
+              title="LOGIN"
+            />
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+export default Signup;

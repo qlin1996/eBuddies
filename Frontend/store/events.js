0f8f002b8a76e7e8dc290e90a6509c84ee7b94e4
Claire Giordano <cgiordano12@fordham.edu>
2020-08-28 18:16:22 -0400

    finished merges

--- Frontend/store/events.js
+++ Frontend/store/events.js
@@ -15,15 +15,14 @@ export const postEvent = (event) => ({
 
 export const getAllEvents = () => async (dispatch) => {
   try {
-    // const { data } = await axios.get("http://localhost:8080/api/events");
-    const { data } = await axios.get("http://192.168.1.3:8081/api/events");
+    const { data } = await axios.get("http://localhost:8080/api/events");
+    // const { data } = await axios.get("http://192.168.1.3:8081/api/events");
 
     return dispatch(getEvents(data));
   } catch (err) {
     console.error(err);
   }
 };
-
 
 export const getUserEvents = (userId) => async (dispatch) => {
   try {
@@ -35,11 +34,11 @@ export const getUserEvents = (userId) => async (dispatch) => {
   } catch (error) {}
 };
 
-export const postNewEvent = (event) => async (dispatch) => {
+export const postNewEvent = (addEventForm) => async (dispatch) => {
   try {
     const { data } = await axios.post(
-      // "http://localhost:8080/api/events",
-      "http://localhost:8081/api/events",
+      "http://localhost:8080/api/events",
+      // "http://localhost:8081/api/events",
       addEventForm
     );
     return dispatch(postEvent(data));

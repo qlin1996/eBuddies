53e33fa991fd9f2319e9a31960caaa3a9810fae3
Qi (Q) Lin <58145741+qlin1996@users.noreply.github.com>
2020-10-09 23:21:46 -0400

    Delete secrets.js

--- Backend/secrets.js
+++
@@ -1,4 +0,0 @@
-process.env.GOOGLE_CLIENT_ID =
-  "958905447792-btonq7mth3qcu7s7t10klummopl4t2n0.apps.googleusercontent.com";
-process.env.GOOGLE_CLIENT_SECRET = "gi8eMa1sykmZvUNwD4dQWVA6";
-process.env.GOOGLE_CALLBACK = "auth/google/callback";
